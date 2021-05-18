2a93ad0d6cfc4694d37dfd5e8e9d907cc63b98d2
krasovskayav <84172590+krasovskayav@users.noreply.github.com>
2021-05-18 23:23:27 +0300

    first_commit

--- 5.js
+++ 5.js
@@ -1,22 +1,65 @@
-let day = "",       // приоритет
-    text = "",      // Текст
-    i = 1,          // счетчик
+let i = 1,          // счетчик
     arrNotes = [],  // массив заметок
     form = document.dwsForm;
 
 function prClick() {
-    day = form.days.value;
-    text = form.message.value;
-    arrNotes.push("" + i +  " " + text + " " + day.bold() + "<hr></td>");
+    let priority = form.priority.value;
+    let text = form.message.value;
+    let note = {
+        id:i,
+        prioritet:priority,
+        text:text,
+        date:new Date(),//Текущая дата 
+    };
+    arrNotes.push(note);
     i++;
     form.reset();
-    const diary = document.getElementById('diary');
-    diary.innerHTML="<table>" + arrNotes.join('') + "</table>";
-}
+    let box = document.createElement("div");
+    box.id = note.id;
+    let noteDate = document.createElement("div");
+    noteDate.innerHTML = note.date;
+    let noteId = document.createElement("div");
+    noteId.innerHTML = note.id;
+    let notePrioritet = document.createElement("div");
+    notePrioritet.innerHTML = note.prioritet;
+    let noteText = document.createElement("div");
+    noteText.innerHTML = note.text;
+    
 
- $(function(){
-        $("#diary").on('blur', function(){
-            $("#textarea").val($(this).text());
-        });
-    })
- 
+    let save = document.createElement("button") 
+    save.innerHTML = "сохранить";
+    save.addEventListener('click',ClickOnSave);
+     function ClickOnSave() { 
+        let newEl = document.createElement("div");
+        newEl.textContent = noteText.textContent;
+        noteText.replaceWith(newEl);
+    }
+      
+    let edit = document.createElement("button")
+    edit.innerHTML = "изменить";
+    edit.addEventListener('click',ClickOnEdit);
+      function ClickOnEdit() { 
+        let newEl = document.createElement("textarea");
+        newEl.textContent = noteText.textContent;
+        noteText.replaceWith(newEl);
+
+    }            
+
+    let deletebtn = document.createElement("button")
+    deletebtn.innerHTML = "удалить";
+    deletebtn.addEventListener('click',ClickOnDeletebtn);
+     function ClickOnDeletebtn() { 
+        noteText.remove();
+    }
+
+    box.appendChild(noteId);
+    box.appendChild(noteText);
+    box.appendChild(notePrioritet);
+    box.appendChild(noteDate);
+    box.appendChild(save);
+    box.appendChild(edit);
+    box.appendChild(deletebtn);
+
+    let contentArea = document.getElementById("contentArea");
+    contentArea.appendChild(box); 
+}
