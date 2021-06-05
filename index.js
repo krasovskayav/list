df854b8209615e6dedd04ecf65302f39976adfca
krasovskayav <krasvi2001vik@gmail.com>
2021-06-06 01:08:33 +0300

    сервер

--- index.js
+++ index.js
@@ -107,3 +107,17 @@ function ClickonPr(event){
        Otrisovka(note);
     });
 }
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({
+        message: 'Привет, мир!'
+    })
+})
+
+app.listen(3000, function () {  
+    console.log('Сервер запущен!');
+})
